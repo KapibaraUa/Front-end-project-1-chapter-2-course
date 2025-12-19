f934dafb1b884b5e7b252175b537ce4e0e838214
Xamilio <Xamilio25@gmail.com>
2025-12-20 01:48:20 +0200

    частично сделал доставку

---
+++ header-nocategory.js
@@ -0,0 +1,21 @@
+const header = `<header>
+    <div class="containerMain">
+        <div class="containerhighttext">
+            <p>New Arrivals</p>
+        </div>
+        <div class="containerlogo">
+        <div class="btnimg">
+                <a href="#"><img  src="assets/img/login.svg" alt=""></a>
+            </div>
+            <div>
+                <img src="assets/img/logo 1.svg" alt="">
+            </div>
+            <div class="btnimg">
+                <a href="#"><img src="assets/img/search.svg" alt=""></a>
+                <a href="#"><img src="assets/img/cart.svg" alt=""></a>
+            </div>
+        </div>
+    </div>
+</header>`;
+
+document.body.insertAdjacentHTML('afterbegin', header);
